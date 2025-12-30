90cc0336cf3b7d5ab7d590304f6bf39ab6b3f66a
salaamii <salamiolamide10@gmail.com>
2025-12-30 21:53:02 +0100

    update trash icon

--- script.js
+++ script.js
@@ -6,23 +6,34 @@ function addTask(){
         alert("Please enter a task");
     }
     else{
+        // create an li and add to ul when the add button is clicked
+
         let li = document.createElement("li");
         li.textContent = input.value;
         listContainer.appendChild(li);
+
+        //CREATE A DELETE ICON WHEN THE LI IS CREATED
+
         let span = document.createElement("span");
-        span.textContent = "\u00d7";
+        span.innerHTML = '<i class="fas fa-trash"></i>';
         li.appendChild(span);
+
+        
+
+        //add event listener to mark the task as completed and uncompleted
 
         li. addEventListener("click", () => {
     li.classList.toggle("checked");
 })
 
-span.addEventListener("click", () => {
+span.addEventListener("click", (e) => {
     li.remove();
      e.stopPropagation();
 })
     }
 
     input.value = '';
+   
 }
+
 
